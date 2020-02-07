b0a180ed5ec08e604a7a8ddc243b5633513ab878
Hajime Murao <2620990+hjmr@users.noreply.github.com>
2020-02-07 18:02:30 +0900

    upload and point the center of the image.

--- static/js/center_point.js
+++ static/js/center_point.js
@@ -3,27 +3,12 @@ let img;
 // Pareters
 let imageFilename = 'chewbacca.jpg';
 
-function calcCanvasSize() {
-    let cnvWidth, cnvHeight;
-    let aratio = windowWidth / windowHeight;
-    if( aratio < origAratio ) {
-        cnvWidth  = windowWidth;
-        cnvHeight = cnvWidth / origAratio;
-    } else {
-        cnvHeight = windowHeight;
-        cnvWidth  = cnvHeight * origAratio;
-    }
-    let cnvSize = {width:cnvWidth, height:cnvHeight};
-    return cnvSize;
-}
-
 function preload() {
     img = loadImage('/imgs/' + imageFilename);
 }
 
 function setup() {
-    cnvSize = calcCanvasSize();
-    let cnv = createCanvas(cnvSize.width, cnvSize.height);
+    let cnv = createCanvas(img.width, img.height);
     cnv.parent('myCanvas');
 }
 
@@ -32,6 +17,10 @@ function draw() {
     line(0, mouseY, width, mouseY);
     line(mouseX, 0, mouseX, height);
     ellipse(mouseX, mouseY, 5, 5);
+}
+
+function mouseClicked() {
+
 }
 
 function initParameters(imageFile) {
